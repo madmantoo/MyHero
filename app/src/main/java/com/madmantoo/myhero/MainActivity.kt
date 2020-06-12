beb166461c88c452b4754ce2f0eb850735989185
madmantoo <rahmadhermanto15@gmail.com>
2020-06-12 20:06:29 +0700

    prepare menu item

--- app/src/main/java/com/madmantoo/myhero/MainActivity.kt
+++ app/src/main/java/com/madmantoo/myhero/MainActivity.kt
@@ -2,6 +2,8 @@ package com.madmantoo.myhero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.madmantoo.myhero.adapter.ListHeroAdapter
@@ -27,5 +29,31 @@ class MainActivity : AppCompatActivity() {
         rvHeroes.layoutManager = LinearLayoutManager(this)
         val listHeroAdapter = ListHeroAdapter(list)
         rvHeroes.adapter = listHeroAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+
+            }
+
+            R.id.action_grid -> {
+
+            }
+
+            R.id.action_cardview -> {
+
+            }
+        }
     }
 }
