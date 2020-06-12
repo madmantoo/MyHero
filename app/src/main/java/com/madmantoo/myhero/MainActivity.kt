28242e30601f447e96ea7e6caebe6cddb891ffef
madmantoo <rahmadhermanto15@gmail.com>
2020-06-12 20:32:19 +0700

    add grid adapter to main activity

--- app/src/main/java/com/madmantoo/myhero/MainActivity.kt
+++ app/src/main/java/com/madmantoo/myhero/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.madmantoo.myhero.adapter.GridHeroAdapter
 import com.madmantoo.myhero.adapter.ListHeroAdapter
 import com.madmantoo.myhero.model.Hero
 import com.madmantoo.myhero.model.HeroesData
@@ -31,6 +33,12 @@ class MainActivity : AppCompatActivity() {
         rvHeroes.adapter = listHeroAdapter
     }
 
+    private fun showRecyclerGrid() {
+        rvHeroes.layoutManager = GridLayoutManager(this,2)
+        val gridHeroAdapter = GridHeroAdapter(list)
+        rvHeroes.adapter = gridHeroAdapter
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return super.onCreateOptionsMenu(menu)
@@ -44,11 +52,11 @@ class MainActivity : AppCompatActivity() {
     private fun setMode(selectedMode: Int) {
         when (selectedMode) {
             R.id.action_list -> {
-
+                showRecyclerList()
             }
 
             R.id.action_grid -> {
-
+                showRecyclerGrid()
             }
 
             R.id.action_cardview -> {
