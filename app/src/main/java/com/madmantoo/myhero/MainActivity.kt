e51a3d485eb35469644d3b4ee9e38d35dc33c5eb
madmantoo <rahmadhermanto15@gmail.com>
2020-06-12 16:51:28 +0700

    initiate recyclerview to main activity

--- app/src/main/java/com/madmantoo/myhero/MainActivity.kt
+++ app/src/main/java/com/madmantoo/myhero/MainActivity.kt
@@ -2,11 +2,30 @@ package com.madmantoo.myhero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.madmantoo.myhero.adapter.ListHeroAdapter
+import com.madmantoo.myhero.model.Hero
+import com.madmantoo.myhero.model.HeroesData
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvHeroes: RecyclerView
+    private var list: ArrayList<Hero> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(HeroesData.listData)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
     }
 }
